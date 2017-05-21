9bd378b42d99a9bfdc415db6768b2a301de1458d
remix187 <robingebrigtsen@gmail.com>
2017-05-21 23:41:49 +0200

    3. Random Updates

--- www/js/fade.js
+++ www/js/fade.js
@@ -1,7 +1,17 @@
 var btn = document.querySelector('.js-btn');
+var btn2 = document.querySelector('.js-btn2');
+var btn3 = document.querySelector('.js-btn3');
 var el = document.querySelector('.js-fade');
 
 btn.addEventListener('click', function(e){
+  el.classList.remove('is-paused');
+});
+
+btn2.addEventListener('click', function(e){
+  el.classList.remove('is-paused');
+});
+
+btn3.addEventListener('click', function(e){
   el.classList.remove('is-paused');
 });
 
--- www/js/nextbtn.js
+++ www/js/nextbtn.js
@@ -26,15 +26,56 @@ document.querySelector(".arrownext").addEventListener("click", function(){
 ******* INTRO 1 ARROWS
 *****************************************************/
 
+/* BACK TO HOME */
 document.querySelector(".arrowprev2").addEventListener("click", function(){
 	
 		mainMenu.classList.remove("hidden");
 		slide1.classList.add("hidden");
 });
+/* END */
 
 
+/* GO TO NEXT */
 document.querySelector(".arrownext2").addEventListener("click", function(){
 	
 		slide1.classList.add("hidden");
 		slide2.classList.remove("hidden");
 });
+/* END */
+
+
+/****************************************************
+******* INTRO 2 ARROWS
+*****************************************************/
+
+/* BACK TO PREVIOUS */
+document.querySelector(".arrowprev3").addEventListener("click", function(){
+	
+		slide1.classList.remove("hidden");
+		slide2.classList.add("hidden");
+
+});
+/* END */
+
+
+/* GO TO NEXT */
+document.querySelector(".arrownext3").addEventListener("click", function(){
+	
+		slide2.classList.add("hidden");
+		slide3.classList.remove("hidden");
+});
+/* END */
+
+
+/****************************************************
+******* INTRO 2 ARROWS
+*****************************************************/
+
+/* BACK TO PREVIOUS */
+document.querySelector(".arrowprev4").addEventListener("click", function(){
+	
+		slide2.classList.remove("hidden");
+		slide3.classList.add("hidden");
+
+});
+/* END */
