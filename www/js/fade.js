ff1abc62b3774aca3254c3f93284d887e23478f8
remix187 <robingebrigtsen@gmail.com>
2017-05-22 18:41:01 +0200

    6. Fixes

--- www/js/fade.js
+++ www/js/fade.js
@@ -1,12 +1,24 @@
 
 var btn = document.querySelector('.js-btn');
+var btn2 = document.querySelector('.js-btn2');
+var btn3 = document.querySelector('.js-btn3');
 var el = document.querySelector('.js-fade');
+var el = document.querySelector('.js-fade2');
+var el = document.querySelector('.js-fade3');
 
 
-btn.addEventListener('click', function(e){
+btn.addEventListener('mousedown', function(e){
   el.classList.remove('is-paused');
 });
 
+
+btn2.addEventListener('mousedown', function(e){
+  el.classList.remove('is-paused2');
+});
+
+btn3.addEventListener('mousedown', function(e){
+  el.classList.remove('is-paused3');
+});
 
 
 /****************************************************
