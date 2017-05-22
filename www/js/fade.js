bad472c72d03a146c34ecbcea997288f7fc6767f
remix187 <robingebrigtsen@gmail.com>
2017-05-22 12:03:33 +0200

    4. Random Updates

--- www/js/fade.js
+++ www/js/fade.js
@@ -1,17 +1,33 @@
+
 var btn = document.querySelector('.js-btn');
-var btn2 = document.querySelector('.js-btn2');
-var btn3 = document.querySelector('.js-btn3');
 var el = document.querySelector('.js-fade');
+
 
 btn.addEventListener('click', function(e){
   el.classList.remove('is-paused');
 });
 
-btn2.addEventListener('click', function(e){
-  el.classList.remove('is-paused');
-});
 
-btn3.addEventListener('click', function(e){
-  el.classList.remove('is-paused');
-});
 
+/****************************************************
+******* Show / hide step content
+*****************************************************/
+
+/*
+
+var btn = document.querySelector('.js-btn');
+var introList = document.querySelectorAll('.js-fade');
+
+for (var i = 0; i < introList.length; i++) {
+
+	var step = introList[i];
+
+	next.addEventListener('mousedown', function (event) {
+
+		var nextIntro = this.querySelector('.js-fade');
+		nextIntro.classList.remove('is-paused');
+
+	});
+};
+
+*/
