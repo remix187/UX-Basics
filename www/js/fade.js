511f7a78bdde0d904b3b23e536cefb6a3fc791d1
remix187 <robingebrigtsen@gmail.com>
2017-05-22 19:30:16 +0200

    7. Bugfixes

--- www/js/fade.js
+++ www/js/fade.js
@@ -1,45 +1,8 @@
 
 var btn = document.querySelector('.js-btn');
-var btn2 = document.querySelector('.js-btn2');
-var btn3 = document.querySelector('.js-btn3');
 var el = document.querySelector('.js-fade');
-var el = document.querySelector('.js-fade2');
-var el = document.querySelector('.js-fade3');
 
 
 btn.addEventListener('mousedown', function(e){
   el.classList.remove('is-paused');
 });
-
-
-btn2.addEventListener('mousedown', function(e){
-  el.classList.remove('is-paused2');
-});
-
-btn3.addEventListener('mousedown', function(e){
-  el.classList.remove('is-paused3');
-});
-
-
-/****************************************************
-******* Show / hide step content
-*****************************************************/
-
-/*
-
-var btn = document.querySelector('.js-btn');
-var introList = document.querySelectorAll('.js-fade');
-
-for (var i = 0; i < introList.length; i++) {
-
-	var step = introList[i];
-
-	next.addEventListener('mousedown', function (event) {
-
-		var nextIntro = this.querySelector('.js-fade');
-		nextIntro.classList.remove('is-paused');
-
-	});
-};
-
-*/
